3567435be70b9582dcc9d6fde6d048d0df86e8ee
Maximilian Kürschner <mail@programonaut.com>
2024-08-18 10:35:30 +0200

    bitwarden user input

--- bitwarden.sh
+++ bitwarden.sh
@@ -1,10 +1,12 @@
+read -p "Enter your bitwarden server [bitwarden.com]: " BITWARDEN_SERVER
+BITWARDEN_SERVER=${BITWARDEN_SERVER:-bitwarden.com}
+
+read -p "Enter your email: " BITWARDEN_EMAIL
+BITWARDEN_EMAIL=${BITWARDEN_EMAIL}
+
 bw config server ${BITWARDEN_SERVER}
 if [[ $BW_SESSION == "" ]]
 then
     export BW_SESSION=$(bw login ${BITWARDEN_EMAIL} --raw)
-fi
-
-if [[ $BW_SESSION == "" ]]
-then
     export BW_SESSION=$(bw unlock --raw)
 fi
