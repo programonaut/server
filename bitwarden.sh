f8161debf2d3ee93764f3ed1fd84637d4fe8bc3c
Maximilian Kürschner <mail@programonaut.com>
2024-08-18 10:52:33 +0200

    zsh compliant

--- bitwarden.sh
+++ bitwarden.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
+if [[ echo $0 | sed "s/-//g" -ne "bash" ]]
+then
+    read "BITWARDEN_SERVER?Enter your bitwarden server [bitwarden.com]: "
+    BITWARDEN_SERVER=${BITWARDEN_SERVER:-bitwarden.com}
 
-read -p "Enter your bitwarden server [bitwarden.com]: " BITWARDEN_SERVER
-BITWARDEN_SERVER=${BITWARDEN_SERVER:-bitwarden.com}
+    read "BITWARDEN_EMAIL?Enter your email: "
+    BITWARDEN_EMAIL=${BITWARDEN_EMAIL}
+else
+    read -p "Enter your bitwarden server [bitwarden.com]: " BITWARDEN_SERVER
+    BITWARDEN_SERVER=${BITWARDEN_SERVER:-bitwarden.com}
 
-read -p "Enter your email: " BITWARDEN_EMAIL
-BITWARDEN_EMAIL=${BITWARDEN_EMAIL}
+    read -p "Enter your email: " BITWARDEN_EMAIL
+    BITWARDEN_EMAIL=${BITWARDEN_EMAIL}
+fi
+
 
 bw config server ${BITWARDEN_SERVER}
 if [[ $BW_SESSION == "" ]]
